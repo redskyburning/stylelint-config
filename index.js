94e233a62202c52479e6c942f657916efd3e9079
Ben Webb <ben.webb@revelsystems.com>
2020-01-07 12:44:36 -0500

    porting

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+	extends: 'stylelint-config-standard',
+	rules: {
+		'indentation': 'tab',
+		'function-name-case': 'lower',
+		'number-leading-zero': 'never',
+		'declaration-colon-space-after': null,
+		'at-rule-no-unknown': null,
+	}
+};

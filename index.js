e12075cdc256581503ffe3ffb4645d2a5c849806
Ben Webb <ben@redskyburning.com>
2020-10-13 12:26:04 -0400

    no important

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
 		'number-leading-zero': 'never',
 		'declaration-colon-space-after': null,
 		'at-rule-no-unknown': null,
+		'declaration-no-important': true,
 		'scss/dollar-variable-colon-space-before': 'never',
 		'scss/at-rule-no-unknown': true,
 		'scss/dollar-variable-pattern': /^[a-z][a-z0-9]+(-[a-z0-9]+)*$/, // Kebab case

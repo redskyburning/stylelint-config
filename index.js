eb32c9f34ac249d93c870e3571f7defb371985a7
Ben Webb <ben@redskyburning.com>
2020-10-13 11:44:37 -0400

    added scss

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ module.exports = {
 	extends: 'stylelint-config-standard',
 	rules: {
 		'indentation': 'tab',
-		'function-name-case': 'lower',
+		'function-name-case': null,
 		'number-leading-zero': 'never',
 		'declaration-colon-space-after': null,
 		'at-rule-no-unknown': null,
-	}
+		'scss/dollar-variable-colon-space-before': 'never',
+	},
+	plugins: [
+		'stylelint-scss',
+	],
 };

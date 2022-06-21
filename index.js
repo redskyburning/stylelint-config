4e91ceb75cf43ed27c7e1bcc0ed1d62d6fc4a627
Ben Webb <webb@groundfloor.us>
2022-06-21 15:09:13 -0400

    v14

--- index.js
+++ index.js
@@ -9,25 +9,24 @@ module.exports = {
 		'declaration-colon-space-after': null,
 		'at-rule-no-unknown': null,
 		'declaration-no-important': true,
+		'max-line-length': null,
 		'scss/dollar-variable-colon-space-before': 'never',
 		'scss/at-rule-no-unknown': true,
 		'scss/dollar-variable-pattern': /^[a-z][a-z0-9]+(-[a-z0-9]+)*$/, // Kebab case
-		'sh-waqar/declaration-use-variable': [
-			[
-				'/color/',
-				'z-index',
-				'font-size',
-				{
-					ignoreValues: [
+		"scale-unlimited/declaration-strict-value": [
+			["/color$/", "z-index", "font-size"],
+			{
+				ignoreValues: {
+					color: [
 						'transparent',
 						'inherit',
-					]
+					],
 				}
-			]
+			},
 		],
 	},
 	plugins: [
 		'stylelint-scss',
-		'stylelint-declaration-use-variable',
+		'stylelint-declaration-strict-value',
 	],
 };
